2d83251e45572d46ef820091e874d701d622b3ea
Mecker-ctrl <abdllaa165@gmail.com>
2023-12-02 11:09:07 +0000

    our work section

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template, request, session, redirect, url_for
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'stardrakeuser@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Cbd492uen!'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
 if __name__ == "__main__":
     app.run(debug=True)
-
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -16,7 +24,16 @@ def index():
 @app.route("/contactus", methods=["GET", "POST"])
 def contactus():
     if request.method == "POST":
-        return 0
+        email = request.form.get("email")
+        message = request.form.get("message")
+
+        msg = Message()
+        msg.recipients = ["abdllaa@gmail.com"]
+        msg.sender = (email)
+        msg.body = (message)
+
+        mail.send(msg)
+        return render_template("contact-us.html")
     else:
         return render_template("contact-us.html")
     
