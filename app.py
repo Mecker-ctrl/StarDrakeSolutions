41f5dd6298bcd8f73cd77c55bb800f523b412cd4
Mecker-ctrl <abdllaa165@gmail.com>
2023-12-05 21:58:32 +0000

    new additions

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'stardrakeuser@gmail.com'
-app.config['MAIL_PASSWORD'] = 'Cbd492uen!'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-mail = Mail(app)
+googleSiteKey = "6LdHnSUpAAAAAA3sP9fFqOTRK2fY6my-kzCrRfhR"
 
 if __name__ == "__main__":
     app.run(debug=True)
@@ -24,15 +17,6 @@ def index():
 @app.route("/contactus", methods=["GET", "POST"])
 def contactus():
     if request.method == "POST":
-        email = request.form.get("email")
-        message = request.form.get("message")
-
-        msg = Message()
-        msg.recipients = ["abdllaa@gmail.com"]
-        msg.sender = (email)
-        msg.body = (message)
-
-        mail.send(msg)
         return render_template("contact-us.html")
     else:
         return render_template("contact-us.html")
@@ -43,3 +27,10 @@ def ourwork():
         return 0
     else:
         return render_template("our-work.html")    
+    
+@app.route("/newsletter", methods=["GET", "POST"])
+def newsletter():
+    if request.method == "POST":
+        return 0
+    else:
+        return render_template("newsletter.html")    
